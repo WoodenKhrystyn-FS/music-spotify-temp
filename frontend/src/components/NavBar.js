c60a8da04b5961a73d896ef196061d99f7254f07
WoodenKhrystyn-FS <kvwooden@student.fullsail.edu>
2025-09-29 11:10:41 -0400

    Added navbar title and wrapped links and logout button in div for better styling

--- frontend/src/components/NavBar.js
+++ frontend/src/components/NavBar.js
@@ -4,23 +4,25 @@ import LogoutBtn from "./LogoutBtn";
 
 function NavBar() {
   //Navigation bar with links to different pages and a logout button
+  //Logout function clears token and redirects back to the login page
 
-  
   return (
     <nav className="navbar" style={styles.navbar}>
-      <Link to="/search" style={styles.link}>
-        Search
-      </Link>
-      <Link to="/protected" style={styles.link} s>
-        Protected
-      </Link>
-      <LogoutBtn />
+      <h2>Spotify Music</h2>
+      <div>
+        <Link to="/search" style={styles.link}>
+          Search
+        </Link>
+        <Link to="/protected" style={styles.link}>
+          Protected
+        </Link>
+        <LogoutBtn />
+      </div>
     </nav>
   );
 }
 
 export default NavBar;
-
 
 //Styles NavBar component
 const styles = {
